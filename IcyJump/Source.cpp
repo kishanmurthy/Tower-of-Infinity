72970311cefaaf29c1180acd38530adfb18a54b3
Kishan N <kishann380@gmail.com>
2017-04-22 12:25:10 +0530

    Added Reshape Function for any Aspect Ratio | Current Aspect Ratio 16:9 .

--- IcyJump/Source.cpp
+++ IcyJump/Source.cpp
@@ -4,12 +4,40 @@
 
 void render()
 {
-	glClearColor(0, 0, 0, 1);
+	glClearColor(0,0, 0, 1);
 	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.5, 0.5, 0);
+	glRectd(6, 2.5, 10, 6.5);
 	glFlush();
 }
 
+void keyboard(unsigned char c,int x,int y)
+{
+	
+}
 
+void reshape(int w,int h)
+{
+	float aspectRatio = 16.0 / 9.0;
+	float aspectRatioDiff= float(w) / float(h) - aspectRatio;
+	float aspectRatioChange = aspectRatioDiff / aspectRatio;
+	if (aspectRatioChange>0)
+	{
+		glViewport((w-aspectRatio*h)/2, 0, aspectRatio*h, h);
+	}
+	else if (aspectRatioChange < 0)
+	{
+		glViewport(0, (h - (1 / aspectRatio)*w)/2, w, (1 / aspectRatio)*w);
+	}
+	glutPostRedisplay();
+}
+void myinit()
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, 16, 0, 9);
+	glMatrixMode(GL_MODELVIEW);
+}
 
 int main(int argc, char *argv[])
 {
@@ -17,9 +45,9 @@ int main(int argc, char *argv[])
 	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
 	glutInitWindowSize(1280, 720);
 	glutCreateWindow("Icy Jump");
+	myinit();
 	glutDisplayFunc(render);
+	glutKeyboardFunc(keyboard);
+	glutReshapeFunc(reshape);
 	glutMainLoop();
-
-
-
 }
